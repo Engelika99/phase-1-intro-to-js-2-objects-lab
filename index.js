a8cd2f047610e304cc9999f30f2306659c29a462
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-03-20 22:41:44 -0400

    Updates objects

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name: "Elmo",
+    streetAddress: "La la la la Elmo's World",
+};
+
+function updateEmployeeWithKeyAndValue(employee, name, value){
+    const employee2 = {...employee};
+    employee2[name] = value;
+    return employee2;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, value){
+    employee[streetAddress] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, name){
+    const employee3 = {...employee};
+    employee3[name];
+    delete employee3.name;
+    return employee3;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, streetAddress){
+    delete employee[streetAddress];
+    return employee;
+}
